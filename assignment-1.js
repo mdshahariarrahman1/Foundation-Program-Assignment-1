902049afd0703d6661396b3e115a3131fd1d2b29
mdshahariar98 <mdsidratulsaimshccho@gmail.com>
2026-09-02 12:24:00 +0600

    Foundation Program assignment-1

---
+++ assignment-1.js
@@ -0,0 +1,98 @@
+// Problem-1
+
+function describeValue  (value)  {
+    const type = typeof value;
+    const truthyOrFalsy = value ? "truthy" : "falsy";
+
+    return `${type} | ${truthyOrFalsy}`;
+};
+
+
+
+// problem-2
+
+function getDayType  (day)  {
+    switch (day.toLowerCase()) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+};
+
+
+
+// Problem-3
+
+function validateUsername  (username)  {
+    if (username.length < 4) {
+        return "Too Short";
+    } else if (username.includes(" ")) {
+        return "No Space Allowed";
+    } else if (username.toLowerCase().includes("admin")) {
+        return "Reserved Word";
+    } else {
+        return "Available";
+    }
+};
+
+
+
+// Problem-4
+
+function getCngFare  (distance, isNight = false, waitingMinutes = 0)  {
+    let fare = 50;
+
+    if (distance > 2) {
+        fare = fare + (distance - 2) * 15;
+    }
+
+    fare = fare + waitingMinutes * 2;
+
+    if (isNight) {
+        fare = fare * 1.20;
+    }
+
+    return fare;
+};
+
+
+
+
+// Problem-5
+
+
+function getChaseVerdict(target, scored, ballsLeft) {
+    const runsNeeded = target - scored;
+
+    if (runsNeeded <= 0) {
+        return "Won";
+    }
+
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
+
+    const requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    let verdict;
+
+    if (requiredRate <= 6) {
+        verdict = "Comfortable";
+    } else if (requiredRate <= 12) {
+        verdict = "Tough";
+    } else {
+        verdict = "Almost Impossible";
+    }
+
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
--- problem-1.js
+++
@@ -1,9 +0,0 @@
-// Problem-1
-
-const describeValue = (value) => {
-    const type = typeof value;
-    const truthyOrFalsy = value ? "truthy" : "falsy";
-
-    return `${type} | ${truthyOrFalsy}`;
-};
-
